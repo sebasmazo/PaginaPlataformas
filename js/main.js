dbddde1a7b3599db241f8f35501538f79eb35b17
sebasmazo <sebastianmazovelez@gmail.com>
2022-01-29 17:34:26 -0500

    Gatos exoticos
    Cambios formulario, clase usuario en JS.
    Más tipos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 function obtenerDatos(params) {
     var nombre = document.getElementById("fname").value;
     var email = document.getElementById("email").value;
-    alert(nombre + ", bienvenido a nuestro blog michero!!!");
-}
+    var razafavorita = document.getElementById("cats").value;
+    const user = {name: nombre, email: email, razafavorita: razafavorita};
+    if (nombre!="" && email!="") {
+        alert(user.name + ", bienvenido a nuestro blog michero!!!");
+    }else{
+        alert("Por favor ingrese los datos para suscribirse");
+    }
+    
+} 
