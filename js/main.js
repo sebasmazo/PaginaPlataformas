f53921ff9a9961667a6aacdda3c7632f74277515
sebasmazo <sebastianmazovelez@gmail.com>
2022-01-29 16:56:57 -0500

    Avances blog, implementación JS

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function obtenerDatos(params) {
+    var nombre = document.getElementById("fname").value;
+    var email = document.getElementById("email").value;
+    alert(nombre + ", bienvenido a nuestro blog michero!!!");
+}
